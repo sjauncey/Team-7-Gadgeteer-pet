4930e61692caa68eee158f2b4b71fe7ca43e0deb
Sam Jauncey <samjauncey@googlemail.com>
2012-04-26 01:07:23 +0100

    MovementController implemented as far as possible without actual testing

--- Project/Project/MovementController.cs
+++ Project/Project/MovementController.cs
@@ -5,7 +5,12 @@ using GT = Gadgeteer;
 
 using Gadgeteer.Modules.Seeed;
 
-//The NC terminal is linked to the positive terminal
+/*Offset is calculated so that if the rat is pointing at local north, normalise will return 45 degrees.
+ * This gets round the problem of the compass value passing from 360 to 0.
+ * 
+ * Both NO terminals are connected to the +ve terminal, and we assume that openning the odd relays is "forward".
+ * Relays 1&2 control the right relay, 3&4 control the left.
+ * */
 
 namespace GadgeteerApp1
 {
@@ -14,13 +19,23 @@ namespace GadgeteerApp1
         private Relays relays;
         private Compass compass;
         private GT.Timer runTimer;
-        private float latestPoll;
-        private float localNorth, localEast, localSouth, localWest;
-        private int cutoff;
+        private float offset;
+        private int objective;
+        private Program model;
 
-        public MovementController(Relays relayBoard, Compass compassBoard){
+        public MovementController(Relays relayBoard, Compass compassBoard, Program passedModel){
+            model = passedModel;
             relays = relayBoard;
             compass = compassBoard;
+            compass.MeasurementComplete +=new Compass.MeasurementCompleteEventHandler(compassContinuousComplete);
+        }
+
+        //There's never any harm in having a destrucor.
+        //Might get rid of it at some point though
+        public ~MovementController(){
+            if(relays.Relay1 || relays.Relay2 || relays.Relay3 || relays.Relay4 || runTimer.IsRunning){
+            //TODO Some error has occured. Handle properly.
+            }
         }
 
         public void advance(){
@@ -30,13 +45,21 @@ namespace GadgeteerApp1
         }
 
         public void rotateRight(){
+            objective = (objective + 90) % 360;
 
-            pollNum = 0;
-            while(pollNum < 2){
-                compass.RequestMeasurement();
-            }
+            relays.Relay2 = true;
+            relays.Relay3 = true;
 
-            int goal = ((store[0] + store[1] + store[2]) / 3) + 90;
+            compass.StartContinuousMeasurements();
+        }
+
+        public void rotateLeft(){
+            objective = (objective + 270) % 360; //Can't just minus 90 as % will give -ve answer on -ve input
+ 
+            relays.Relay1 = true;
+            relays.Relay4 = true;
+
+            compass.StartContinuousMeasurements();
         }
 
 
@@ -45,9 +68,23 @@ namespace GadgeteerApp1
             runTimer.Tick += new GT.Timer.TickEventHandler(runTimer_Tick);
         }
 
-        public void getNorth(){
-            float f = pollCompass();
-            if (345< f && 355 > f){ cutoff = 0;
+        public void setOffset(){
+            compass.MeasurementComplete += new Compass.MeasurementCompleteEventHandler(compassDiscreteComplete);
+            compass.RequestMeasurement();
+            compass.MeasurementComplete -= compassDiscreteComplete;
+        }
+
+        void compassDiscreteComplete(Compass sender, Compass.SensorData sensorData){
+            offset = 45 - sensorData.X;
+        }
+
+        void compassContinuousComplete(Compass sender, Compass.SensorData sensorData){
+            int value = (int)localise(sensorData.X);
+            if (Math.Abs(value - objective) < 3){
+                stop();
+                compass.StopContinuousMeasurements();
+                model.movementFinished();
+            }
         }
 
         public void allStop(){
@@ -58,27 +95,14 @@ namespace GadgeteerApp1
         private void runTimer_Tick(GT.Timer timer){
             stop();
             runTimer.Stop();
+            model.movementFinished();
         }
 
-        private float pollCompass(int passes){
-            float total = 0;
-
-            for(int i=0;i<passes;i++){
-                compass.RequestMeasurement();
-                total += latestPoll;
-            }
-
-            return (total / passes);
+        private float localise(float measure){
+            return (measure + offset) % 360;
         }
 
-        private float pollCompass(){
-            compass.RequestMeasurement();
-            return latestPoll;
-        }
-
-
-        private void stop()
-        {
+        private void stop(){
             relays.Relay1 = false;
             relays.Relay2 = false;
             relays.Relay3 = false;
